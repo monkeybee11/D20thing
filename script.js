40365631669d7a131579d8afae13e3d596c732b5
monkeybee11 <monkeybee1@googlemail.com>
2022-12-01 18:26:13 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,5 @@ function diceroll(gif1){
 		gif1.classList.add("png"+mince_pie);
 		gif1.classList.remove("roll"+mince_pie);
 		rollbut.classList.remove("startroll");
-		rollbut.classList.add("afterroll");
-	}, 5000);
+	}, 4000);
 	}
